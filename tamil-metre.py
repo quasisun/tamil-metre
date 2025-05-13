3e4063bb264359d4403cdc70b32e159bbf3f932f
quasisun <svetlana@sri.auroville.org>
2025-05-13 16:16:04 +0200

    Update tamil-metre.py

--- tamil-metre.py
+++ tamil-metre.py
@@ -1,13 +1,3 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 # Ven\uc0\u817 p\u257  Meter Visualiser  \'96  Tamil n\u275 r / nirai\
-# (c) 2025  Svetlana Kreuzer\
-\
 import streamlit as st\
 import matplotlib.pyplot as plt\
 from matplotlib.patches import Rectangle, Patch\
